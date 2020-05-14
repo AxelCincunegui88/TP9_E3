202e7fd2da3c6d9f3622bd98c685698b447a9bd4
axel <axel@axel-R430-R480>
2020-05-14 16:29:26 -0300

    probando

---
+++ suma_digitos.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   suma_digitos.h
+ * Author: axel
+ *
+ * Created on 14 de mayo de 2020, 16:25
+ */
+
+#ifndef SUMA_DIGITOS_H
+#define SUMA_DIGITOS_H
+
+
+
+
+#endif /* SUMA_DIGITOS_H */
+
